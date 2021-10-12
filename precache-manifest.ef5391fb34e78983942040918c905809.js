8f5476f7314c989b8fb92715218d202c93c7d359
Zachary Lin <zcl217@nyu.edu>
2021-10-13 01:20:52 +0900

    Update: site

---
+++ precache-manifest.ef5391fb34e78983942040918c905809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f035f46de794670482aefd20997f44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d303e99b9bb7980a688",
+    "url": "/static/css/main.9a2c1514.chunk.css"
+  },
+  {
+    "revision": "9212955ec476750b2e86",
+    "url": "/static/js/2.af56056c.chunk.js"
+  },
+  {
+    "revision": "6d303e99b9bb7980a688",
+    "url": "/static/js/main.e6abe60e.chunk.js"
+  },
+  {
+    "revision": "b208075b390259fb6f8e",
+    "url": "/static/js/runtime~main.d5cb73d9.js"
+  }
+]);
